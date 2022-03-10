f6fe3954805e0448300b3e7e1d1c801486c9c2b3
mpflynnx <mpflynnx@gmail.com>
2022-03-10 12:22:25 +0000

    feat: automatic config file creation plus more
    adds automatic config file creation/updating
    adds directory path creation
    updates to README.md including new demo videos

--- README.md
+++ README.md
@@ -1,17 +1,78 @@
 
 # clip2file
 
-clip2file is a Python command line application, which will write the contents of the clipboard to a new file, using preset arguments to define file location and filename text.
+clip2file is a Python command line application, which will write text from the clipboard to a new file.
+
+
+## Features
+
+- Writes text from the primary clipboard to a new text file
+- Creates the directory path to the new file (if required).
+- Creates a directory lookup name, which can be used to save typing the full directory path on successive runs.
+- Automatically creates/updates a config file as required.
+- Automatically sanitizes the new files filename.
+- Automatically prepends date to new files filename.
+- Removes any emoji from the clipboard text prior to writing to the file.
 
 ## Demo
 
+![Example](./readme_resources/termtosvg_bzl9wjja.svg)
 
-![Example](./readme_resources/termtosvg_y12qwm80.svg)
+![Example](./readme_resources/termtosvg_nvnljepz.svg)
 
 
+## Support
 
+Only tested on Linux OS running Python 3.7.2
+
+On Linux, this application makes use of the xsel command, which should come with the os. Otherwise run "sudo apt-get install xsel" or search your distrubutions package repository.
+
+
+## Installation
+
+Using the linux command line. Install clip2file from github into a Python virtual environment.
+
+```bash
+  cd ~
+  python3 -m venv .venv        # create virtual environment
+  source .venv/bin/activate    # activate environment
+  python3 -m pip install git+https://github.com/mpflynnx/clip2file.git     # install from git
+
+```
+    
+## Usage/Examples
+
+Copy text to the clipboard from any available source, i.e. webpage, pdf or document.
+
+On the Linux command line enter:
+
+```bash
+clip2file <lookup name or directory path to store the new file> "text describing the contents of file"
+
+# using the path to directory 'notes' in /home/username.
+# thereafter lookup name 'notes' can be used instead of the long directory path.
+clip2file a/directory/deep/in/my/home/for/notes "some notes on subject xyz"
+
+# Using lookup name 'notes'
+clip2file notes "some more notes on subject xyz"
+```
+
+
+## Roadmap
+
+- Microsoft Windows support.
+- Auto preview of new file contents.
+- Append to existing file if filenames match.
+- Auto wrap text in file.
+- Allow a change of default extension from .txt
+- Tests
+
+
+## Badges
 
 [![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
 [![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
 [![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
 [![Conventional Commits](https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg)](https://conventionalcommits.org)
+
+
--- src/clip2file/clip2file.py
+++ src/clip2file/clip2file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import sys
 from datetime import date
 from pathlib import Path
 from typing import NamedTuple
@@ -118,9 +119,9 @@ def get_args() -> Args:
     return Args(args.positional1, args.positional2, args.list)
 
 
-def read_dict(path):
+def read_config(path):
     """
-    Returns dictionary, read from yaml file found at 'path'.
+    Returns dictionary, read from yaml config file found at 'path'.
     """
     with open(path, mode="r") as stream:
         try:
@@ -132,17 +133,166 @@ def read_dict(path):
 
 def config_check():
     """
-    Validate the config file.
+    Validate the config file layout and content. Returns config.
     """
 
-    msg = f" \n No config file found at '{config_path}'.\n\n Open a terminal and paste the following.\n\ncat << EOF > {config_path}\nlookup:\n    documents: {Path.home()}/Documents\nEOF\n"
-
-    if not config_path.is_file():
-        print(msg)
-        exit()
+    if not config_path.is_file():  # not exists
+        print("\n It looks like this is the first run.\n")
+        print(f""" Creating a new config file at "{config_path}".\n""")
+        config = empty_config()
+        makedir(config_path.parent)
+        dumpdict(config, config_path)
+        return config
     else:
-        return read_dict(config_path)
+        check_config = read_config(config_path)
+        if check_config is not None:  # not empty file
+            if "lookup" not in check_config.keys():
+                new_config = empty_config()
+                print(
+                    "\n Older config file layout detected, updating config to new layout."
+                )
+                new_config["lookup"].update(check_config)
+                dumpdict(new_config, config_path)
+                return new_config  # when updated
+            return check_config  # when latest
+        return empty_config()  # when empty
 
+
+def define_save_location(raw_pos1_arg):
+    """
+    Validate pos1_arg argument then returns a <class 'pathlib.PosixPath'> object for the "save_location".
+    """
+    # in work below
+    if raw_pos1_arg in parsed_config["lookup"].keys():  # key present?
+        if not get_path(raw_pos1_arg).is_dir():
+            config_error_01(raw_pos1_arg)
+        return get_path(raw_pos1_arg)
+    else:  # not key, a directory
+        pos1_arg = validate_path(raw_pos1_arg)
+        key_name = pos1_arg.name.lower()
+        if key_name in parsed_config["lookup"].keys():
+            if parsed_config["lookup"].get(key_name) == str(pos1_arg):
+                if not get_path(key_name).is_dir():
+                    config_error_01(key_name)
+                return get_path(key_name)
+            else:
+                config_error_02(pos1_arg)
+                return get_path(key_name)
+        else:
+            dir_check(pos1_arg)
+            dumpdict(parsed_config, config_path)
+            return get_path(key_name)
+
+
+def get_path(key_name):
+    """
+    Returns a <class 'pathlib.PosixPath'> object of dict key "key_name" value.
+    """
+    return Path(parsed_config["lookup"].get(key_name))
+
+
+def config_error_01(key_name):
+    """
+    Resolves config file error, when directory path is missing from file system.
+    """
+    print("\n ATTENTION! Config file error 01 detected.\n")
+    dir_check(Path(parsed_config["lookup"].get(key_name)))
+
+
+def config_error_02(key_name):
+    """
+    Resolves config error when file system directory names are duplicated at multiple paths.
+    """
+    print("\n ATTENTION! Config file error 02 detected.")
+    print(
+        f""" "{key_name.name.lower()}" matches "{parsed_config['lookup'].get(key_name.name.lower())}" not "{str(key_name)}"."""
+    )
+    answer = input(" Update config file? [yN]\n")
+    if answer.lower().startswith("y"):
+        print(f"""\n Using directory path "{str(key_name)}".""")
+        parsed_config["lookup"].update(create_dict_entry(key_name))
+        dir_check(key_name)
+        dumpdict(parsed_config, config_path)
+    else:
+        print(
+            f"""\n Using directory path "{parsed_config['lookup'].get(key_name.name.lower())}"."""
+        )
+
+
+def dir_check(path):
+    """
+    Check existence of directory 'path' a <class 'pathlib.PosixPath'> object,
+    create it if it does not exist. Add to dict 'config:lookup' if it does exist.
+    """
+
+    msg1 = f"""\n Directory path "{path}" exists.\n"""
+    msg2 = f""" You can use 'clip2file {path.name.lower()}' next time.\n"""
+
+    if path.is_dir():
+        print(msg1)
+        print(msg2)
+    else:
+        if not parsed_config["defaults"]["force-creation-of-new-directories"]:
+            answer = input(
+                f'\n Directory path "{path}" does not exist, create it? " [yN]\n'
+            )
+            if not answer.lower().startswith("y"):
+                sys.exit("\n Bye!\n")
+        makedir(path)
+        print(msg2)
+    parsed_config["lookup"].update(create_dict_entry(path))
+
+
+def dumpdict(dict_name, path):
+    """
+    dump dict to yaml config file
+    """
+    with open(path, mode="w", encoding="utf-8") as file:
+        # add comment line in file
+        file.write("# clip2file config file, autogenerated, do not edit.\n")
+        file.write("\n")
+        yaml.safe_dump(dict_name, file, sort_keys=True, indent=4)
+        print(f""" Config file "{path}" updated!\n""")
+
+
+def makedir(dir_path):
+    """
+    Make new directory only if name doesn't exit as a file or directory.
+    """
+    if dir_path.is_file():
+        sys.exit(
+            f"""Fatal error, wanting to create directory "{dir_path}"
+            but this exists already as a file.\n
+            Please delete file "{dir_path}" first or use a different name.
+            """
+        )
+    print(f""" Directory path "{dir_path}" created!\n""")
+    Path.mkdir(dir_path, parents=True, exist_ok=True)
+
+
+def create_dict_entry(path):
+    """
+    Creates the dictionary key, value pair
+    from 'path' a <class 'pathlib.PosixPath'> object.
+    """
+
+    key = path.name.lower()
+    value = str(path.resolve())
+
+    return {key: value}
+
+
+def empty_config():
+    """
+    Create new empty dictionary for config file.
+    """
+    config = {}
+    config["lookup"] = {}
+    config["defaults"] = {"force-creation-of-new-directories": False}
+    return config
+
+
+parsed_config = config_check()
 
 # --------------------------------------------------
 def main() -> None:
@@ -151,8 +301,6 @@ def main() -> None:
     flag_arg = args.list
     raw_pos1_arg = args.positional1
     pos2_arg = args.positional2
-
-    parsed_config = config_check()
 
     try:
         if flag_arg is True:
@@ -170,17 +318,9 @@ def main() -> None:
         if pos2_arg is not None:
             description = sanitize_filename(pos2_arg[0:71], platform="windows")
 
-        pos1_arg = validate_path(raw_pos1_arg)
-
-        if pos1_arg.name.lower() in parsed_config["lookup"]:
-            save_location = Path(parsed_config["lookup"][pos1_arg.name.lower()])
-        else:
-            raise ValueError(
-                f"""Not a valid first positional argument! try one of these: {list(parsed_config["lookup"].keys())}"""
-            )
-
         f_ext = ".txt"
         filename = d1 + c + to_url_style(description) + f_ext  # build new filename
+        save_location = define_save_location(raw_pos1_arg)
         destination = save_location / filename  # Define destination filename
         rawcontent = pyperclip.paste()
 
@@ -197,10 +337,10 @@ def main() -> None:
                 fib.write(content)  # paste contents of clipboard
                 clear_clip()
         else:
-            raise ValueError("Looks like a file of that name already exists!")
+            raise ValueError(" Looks like a file of that name already exists!\n")
 
-        print("New file created, at....")
-        print(f"{destination}")
+        print(" New file created, at....")
+        print(f" {destination}\n")
 
     except ValueError as e:
         exit(str(e))
